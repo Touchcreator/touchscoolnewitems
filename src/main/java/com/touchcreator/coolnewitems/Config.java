2a1ebee830759d2872c04807852e34b6ad1323ee
Touchcreator <tochiokl62@gmail.com>
2025-06-08 15:31:05 -0400

    first commit (again?)

--- src/main/java/com/touchcreator/coolnewitems/Config.java
+++ src/main/java/com/touchcreator/coolnewitems/Config.java
@@ -12,6 +12,8 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
+// holup
+
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
 @EventBusSubscriber(modid = TouchsCoolNewItems.MODID, bus = EventBusSubscriber.Bus.MOD)
